a35b25697abd744024af0a7dc5f1e51719eb10ab
黄云龙 <hylepik@163.com>
2021-04-19 17:40:17 +0800

    说明

--- MVC/src/entry.js
+++ MVC/src/entry.js
@@ -6,3 +6,7 @@ let view = new View()
 let model = new Model()
 let control = new Control(model, view)
 control.rander()
+
+// Control主要处理view层的事件注册及相关业务逻辑，并将view挂载到根元素上
+// model主要处理页面源数据及相关处理方法
+// view主要处理页面模版相关内容
